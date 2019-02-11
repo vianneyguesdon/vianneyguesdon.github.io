5cc3b856dfaaeace82fde9bbaf09b5e2a20a828f
Vianney <vianney.guesdon.dev@gmail.com>
2019-02-12 00:23:12 +0100

    commit 20

--- bloom/js/main.js
+++ bloom/js/main.js
@@ -1,31 +1,20 @@
-//FILTER CARD//
 $(document).ready(function(){
+
+    //FILTER CARDS//
     console.log('jquery loaded')
     $('#search').keyup(function (){
         $('.card-container').removeClass('d-none');
         var filter = $(this).val(); // get the value of the input, which we filter on
         $('.container').find('.card .card-body:not(:contains("'+filter+'"))').parent().parent().addClass('d-none');
     });
+
+    //FLIP CARD//
+    $('.flip-card').flip({
+      axis: 'x',
+      trigger: 'click',
+      speed: 350,
+    });
+
 });
 
 
-//FLIP CARD//
-var card = document.querySelector(".flip-card");
-var playing = false;
-
-card.addEventListener('click',function() {
-  if(playing)
-    return;
-  
-  playing = true;
-  anime({
-    targets: card,
-    scale: [{value: 1}, {value: 1.15}, {value: 1, delay: 150}],
-    rotateY: {value: '+=180', delay: 200},
-    easing: 'easeInOutSine',
-    duration: 500,
-    complete: function(anim){
-       playing = false;
-    }
-  });
-});
