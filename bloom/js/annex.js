e3d48665a1119abb545bf2ec8e358f53799a7a25
Vianney <vianney.guesdon.dev@gmail.com>
2019-02-15 11:05:53 +0100

    commit 21 bloom

---
+++ bloom/js/annex.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    //FLIP CARD//
+    $('.flip-card').flip({
+        axis: 'x',
+        trigger: 'click',
+        speed: 350,
+    });
+});
--- bloom/js/main.js
+++ bloom/js/main.js
@@ -1,20 +1,10 @@
 $(document).ready(function(){
-
     //FILTER CARDS//
-    console.log('jquery loaded')
     $('#search').keyup(function (){
         $('.card-container').removeClass('d-none');
         var filter = $(this).val(); // get the value of the input, which we filter on
         $('.container').find('.card .card-body:not(:contains("'+filter+'"))').parent().parent().addClass('d-none');
     });
-
-    //FLIP CARD//
-    $('.flip-card').flip({
-      axis: 'x',
-      trigger: 'click',
-      speed: 350,
-    });
-
 });
 
 
